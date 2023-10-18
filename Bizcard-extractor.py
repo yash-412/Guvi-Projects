cbfd42ecbf57cd64128c1906637847be94daf6bf
yash-412 <yash.tamil.5@gmail.com>
2023-10-18 12:20:33 +0530

    Update Bizcard-extractor.py

--- Bizcard-extractor.py
+++ Bizcard-extractor.py
@@ -1,86 +1,42 @@
 import streamlit as st
 import cv2
 import numpy as np
-from PIL import Image
 import easyocr
+import pandas as pd
+from PIL import Image
+from spellchecker import SpellChecker
 import re
+from sqlalchemy import create_engine, Column, Integer, String, Text, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+import pymysql
 
-# Initialize EasyOCR
-reader = easyocr.Reader(['en'])
+db_user = 'root'
+db_password = '+91naveen'
+db_host = 'localhost'
+db_port = 3306  # your database port
+db_name = 'business cards'
 
-# Function to apply image processing based on user input
-def process_image(image, sharpness, brightness, contrast, threshold):
-    # Convert PIL image to OpenCV format
-    image = np.array(image)
-    
-    # Apply sharpening
-    kernel = np.array([[-1, -1, -1], [-1, sharpness, -1], [-1, -1, -1]])
-    sharpened_image = cv2.filter2D(image, -1, kernel)
-    
-    # Adjust brightness and contrast
-    enhanced_image = cv2.convertScaleAbs(sharpened_image, alpha=contrast, beta=brightness)
-    
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2GRAY)
-    
-    # Apply Gaussian blur to reduce noise
-    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-    
-    # Apply adaptive thresholding
-    thresholded_image = cv2.adaptiveThreshold(blurred_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, threshold)
-    
-    return thresholded_image
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-def extract_info(text):
-    info = {
-        "name": name,
-        "designation": designation,
-        "mobile": [],
-        "email": "",
-        "website": "",
-        "company": company,
-        "address": ""
-    }
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    # Define regular expressions for each type of information
-    mobile_pattern = r'(?:\+\d{1,4}-\d{1,4}-\d{1,10}|\d{1,4}-\d{1,4}-\d{1,10})'
-    email_pattern = r'[A-Za-z0-9_.]+@[A-Za-z0-9.-]+'
-    website_pattern = r'[Ww][Ww][Ww](.*?)\.com'
-    address_pattern = r'.*'  # Match anything for the address
+metadata = MetaData()
 
-    # Extract mobile numbers (multiple)
-    mobile_matches = re.findall(mobile_pattern, text)
-    info["mobile"].extend(mobile_matches)
-    text = re.sub(mobile_pattern, "", text).strip()
+business_cards = Table(
+    'business_cards',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255)),
+    Column('designation', String(255)),
+    Column('mobile', Text),
+    Column('email', String(255)),
+    Column('website', String(255)),
+    Column('company', String(255)),
+    Column('address', String(255))
+)
 
-    # Extract email
-    email_match = re.search(email_pattern, text)
-    if email_match:
-        info["email"] = email_match.group()
-        text = text.replace(info["email"], "").strip()
-
-    # Extract website
-    website_match = re.search(website_pattern, text)
-    if website_match:
-        info["website"] = website_match.group()
-        text = text.replace(info["website"], "").strip()
-
-    text = text.replace(info["name"], "").strip()
-    text = text.replace(info["designation"], "").strip()
-    text = text.replace(info["company"], "").strip()
-
-    # The remaining text is considered the address
-    info["address"] = text.strip()
-
-    return info
-
-def extract_text(result):
-    text_ = ""
-
-    for detection in result:
-        (text_bbox, text, prob) = detection
-        text_ += text + " "
-    return text_
+metadata.create_all(engine)
 
 def clean_text(image, left_region, right_region):
     result = reader.readtext(image)
@@ -93,48 +49,30 @@ def clean_text(image, left_region, right_region):
 
     return result, left_result, right_result, left_text, right_text
 
-def extract_info(text):
-    info = {
-        "name": name,
-        "designation": designation,
-        "mobile": [],
-        "email": "",
-        "website": "",
-        "company": company,
-        "address": ""
-    }
+def spell_check(text, exceptions=[]):
+    # Spell correction
+    spell = SpellChecker()
+    
+    # Add exceptions to the known words
+    for word in exceptions:
+        spell.known([word])
+    
+    words = text.split()
+    corrected_words = [spell.correction(word) for word in words]
+    corrected_text = ' '.join(corrected_words)
+    
+    # Remove trailing white spaces
+    corrected_text = re.sub(r'\s+$', '', corrected_text)
+    
+    return corrected_text
 
-    # Define regular expressions for each type of information
-    mobile_pattern = r'(?:\+\d{1,4}-\d{1,4}-\d{1,10}|\d{1,4}-\d{1,4}-\d{1,10})'
-    email_pattern = r'[A-Za-z0-9_.]+@[A-Za-z0-9.-]+'
-    website_pattern = r'[Ww][Ww][Ww](.*?)\.com'
-    address_pattern = r'.*'  # Match anything for the address
+def extract_text(result):
+    text_ = ""
 
-    # Extract mobile numbers (multiple)
-    mobile_matches = re.findall(mobile_pattern, text)
-    info["mobile"].extend(mobile_matches)
-    text = re.sub(mobile_pattern, "", text).strip()
-
-    # Extract email
-    email_match = re.search(email_pattern, text)
-    if email_match:
-        info["email"] = email_match.group()
-        text = text.replace(info["email"], "").strip()
-
-    # Extract website
-    website_match = re.search(website_pattern, text)
-    if website_match:
-        info["website"] = website_match.group()
-        text = text.replace(info["website"], "").strip()
-
-    text = text.replace(info["name"], "").strip()
-    text = text.replace(info["designation"], "").strip()
-    text = text.replace(info["company"], "").strip()
-
-    # The remaining text is considered the address
-    info["address"] = text.strip()
-
-    return info
+    for detection in result:
+        (text_bbox, text, prob) = detection
+        text_ += text + " "
+    return text_
 
 def extract_text_lines(list_of_lists):
     # Initialize an empty list to store extracted text line by line
@@ -150,119 +88,143 @@ def extract_text_lines(list_of_lists):
 
     return extracted_text_lines
 
-def split_image(processed_image):
-    split_x = processed_image.shape[1] // 2
+def extract_info(text):
+    info = {
+        "name": name,
+        "designation": designation,
+        "mobile": [],
+        "email": "",
+        "website": "",
+        "company": company,
+        "address": ""
+    }
 
-    # Define padding to be added to the right region (adjust as needed)
-    left_padding = 40
-    right_padding = 40
+    # Define regular expressions for each type of information
+    mobile_pattern = r'(?:\+\d{1,4}-\d{1,4}-\d{1,10}|\d{1,4}-\d{1,4}-\d{1,10})'
+    email_pattern = r'[A-Za-z0-9_.]+@[A-Za-z0-9.-]+'
+    website_pattern = r'[Ww][Ww][Ww](.*?)com'
+    address_pattern = r'.*'  # Match anything for the address
 
-    # Split the image into left and right regions with padding
-    left_region = processed_image[:, :split_x - left_padding:]
-    right_region = processed_image[:, split_x - right_padding:]
+    # Extract mobile numbers (multiple)
+    mobile_matches = re.findall(mobile_pattern, text)
+    info["mobile"].extend(mobile_matches)
+    text = re.sub(mobile_pattern, "", text).strip()
 
-    return left_region, right_region
+    # Extract email
+    email_match = re.search(email_pattern, text)
+    if email_match:
+        info["email"] = email_match.group()
+        text = text.replace(info["email"], "").strip()
 
-# Streamlit app
-st.title("Test Bixcard-X")
+    # Extract website
+    website_match = re.search(website_pattern, text)
+    if website_match:
+        info["website"] = website_match.group()
+        text = text.replace(info["website"], "").strip()
 
-# Define columns for layout
-left_column, right_column = st.columns(2)
+    text = text.replace(info["name"], "").strip()
+    text = text.replace(info["designation"], "").strip()
+    text = text.replace(info["company"], "").strip()
 
-# Upload an image
-uploaded_image = left_column.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    # The remaining text is considered the address
+    info["address"] = text.strip()
 
-pil_image = Image.open(uploaded_image)
+    return info
 
-reset_button = st.button("Reset")
+exceptions = ['BORCELLE','borcelle']
 
-# Default slider values
-default_sharpness = 9.0
-default_brightness = 1.0
-default_contrast = 1.0
-default_threshold = 2.0
+reader = easyocr.Reader(['en'])
 
-if reset_button:
+image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    # Reset sliders to default values
-    sharpness = default_sharpness
-    brightness = default_brightness
-    contrast = default_contrast
-    threshold = default_threshold
+image = Image.open(image)
 
-# Sliders for adjusting parameters for the selected region
-sharpness = 9.0
-brightness = 1.0
-contrast = 1.0
-threshold = 2.0
+image = np.array(image)
 
-processed_image = process_image(pil_image, sharpness, brightness, contrast, threshold)
+scale_factor = 1  # You can adjust this value as needed
+image = cv2.resize(image, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
 
-left_region, right_region = split_image(processed_image)
+thresholded_image = cv2.addWeighted(image, 2.3, np.zeros(image.shape, image.dtype), 0, 10)
 
-# Create select boxes for left and right regions
-selected_region = st.selectbox("Select Region to Edit", ["Left Region", "Right Region"])
+thresholded_image = cv2.convertScaleAbs(thresholded_image, alpha=1.5, beta=50) 
 
-# Define the regions based on user selection
-if selected_region == "Left Region":
-    region = left_region
-    column = left_column
+# sharpening for better precision
+sharpened_image = cv2.filter2D(image, -1, kernel=np.array([[-1, -1, -1],[-1,  9, -1],[-1, -1, -1]]))
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(sharpened_image, cv2.COLOR_BGR2GRAY)
+
+# Apply Gaussian blur to reduce noise
+thresholded_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+# Apply adaptive thresholding for better contrast
+# thresholded_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+split_x = thresholded_image.shape[1] // 2
+
+# Define padding to be added to the right region (adjust as needed)
+left_padding = 15
+right_padding = 30
+
+# Split the image into left and right regions with padding
+left_region = image[:, :split_x - left_padding:] # new padding added
+right_region = image[:, split_x - right_padding:]
+
+result, left_result, right_result, left_text, right_text = clean_text(thresholded_image, left_region, right_region)
+
+# left side has company name
+# other details on right side
+if len(left_result) < len(right_result):  
+    info_result = right_result
 else:
-    region = right_region
-    column = left_column
+    info_result = left_result
 
-# Sliders for adjusting parameters for the selected region
-sharpness = column.slider("Sharpness", 1.0, 20.0, default_sharpness, step=0.1)
-brightness = column.slider("Brightness", -10.0, 10.0, default_brightness, step=0.1)
-contrast = column.slider("Contrast", -10.0, 10.0, default_contrast, step=0.1)
-threshold = column.slider("Threshold", -10.0, 10.0, default_threshold, step=2.0)
+if len(left_text) < len(right_text):
+    company = left_text  
+    info_text = right_text
+else:
+    company = right_text 
+    info_text = left_text
 
-if uploaded_image is not None:
+new_info_text = extract_text_lines(info_result)
 
-    # Read the uploaded image as a PIL image
-    pil_image = Image.open(uploaded_image)
+st.image(image, caption = "Uploaded Image", use_column_width=True)
 
-    # Apply image processing
-    processed_image = process_image(pil_image, sharpness, brightness, contrast, threshold)
-    
-    split_x = processed_image.shape[1] // 2
+name = new_info_text[0]
+designation = new_info_text[1]
+name, designation, company
 
-    # Define padding to be added to the right region (adjust as needed)
-    left_padding = 40
-    right_padding = 40
+x = extract_info(info_text)
 
-    # Split the image into left and right regions with padding
-    left_region = processed_image[:, :split_x - left_padding:]
-    right_region = processed_image[:, split_x - right_padding:]
+corrected_website = x['website'].lower()
+corrected_email = x['email'].lower()
+x['address'] = re.sub(r'\bSt\b|,,|;', lambda match: 'st.' if match.group() == 'St' else ',', x['address'])
+corrected_company = re.sub(r' " ' , '', x['company'])
+corrected_website = re.sub(r'\s', '.', x['website'])
 
-    left_column.image(pil_image, caption = "Original Image", use_column_width=True)
+x['website'] = corrected_website
+x['email'] = corrected_email
+x['company'] = corrected_company.upper()
+x['website'] = corrected_website.lower()
+x['mobile'] = ', '.join(x['mobile'])
+# x['company'] = spell_check(x['company'], exceptions)
 
-    # Display the original and processed images on the right column
-    right_column.image([left_region, right_region], caption=["Left Region", "Right Region"], use_column_width=True)
+if not re.search(r'\.com', x['website']):
+    x['website'] = re.sub(r'(www\..*?)(com)', r'\1.\2', x['website'])
 
-    result, left_result, right_result, left_text, right_text = clean_text(processed_image, left_region, right_region)
+df = pd.DataFrame([x])
 
-    if len(left_result) < len(right_result):  
-        info_result = right_result
-    else:
-        info_result = left_result
+table_name = 'business card'
 
-    if len(left_text) < len(right_text):
-        company = left_text  
-        info_text = right_text
-    else:
-        company = right_text 
-        info_text = left_text
+st.write(df)
 
-    image_text_lines = extract_text_lines(info_result) # extract_text function is deemed unnecessary
+if st.button("Insert Data into Database"):
+    # Insert the data into the table
+    insert_statement = business_cards.insert().values(**x)
+    session.execute(insert_statement)
+    st.success("Data Inserted Data into Database")
+session.commit()
 
-    name = image_text_lines[0]
-    designation = image_text_lines[1]
-
-    image_text = extract_text(info_result)
-
-    main_df = extract_info(image_text)
-    
-    st.write(main_df)
+session.close()
 
 # streamlit run C:\Users\Yash\test_image_extract.py
